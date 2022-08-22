e5d33d5234bbc8d493f5a9abbb4f04c2014b4474
Lexxyla <bamideleadefolaju@gmail.com>
2022-08-22 06:26:02 -0700

    fix betty and create new tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,13 +10,13 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+	binary_tree_t *new;
 
-    new = malloc(sizeof(binary_tree_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = value;
-    new->parent = parent;
-    new->left = NULL;
-    new->right = NULL;
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 }
