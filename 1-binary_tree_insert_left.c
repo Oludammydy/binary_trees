b8bda31725e16d5625fc2975be9505daa92b8ff9
lexxyla <bamideleadefolaju@gmail.com>
2022-08-23 09:46:28 +0100

    fix identifiers

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@
  *         NULL if [arent is NULL
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,7 +10,7 @@
  *         NULL if [arent is NULL
  */
 
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
