84ed10b707bb280833c83a4ce6eacefe206daa86
Oludammydy <oludammydy@gmail.com>
2022-08-24 07:13:50 -0700

    File for task 11 added

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,5 +11,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return (binary_tree_size(tree->right) + (tree->left));
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
