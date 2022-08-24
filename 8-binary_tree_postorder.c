97a5f24e0c8f3827fa5157536af09ea67491c54f
lexxyla <bamideleadefolaju@gmail.com>
2022-08-24 07:31:38 +0100

    traversing

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,10 +7,10 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && func)
-	{
-		binary_tree_postorder(tree->left, func);
-		binary_tree_postorder(tree->right, func);
-		func(tree->n);
-	}
+	if (!tree || !func)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
