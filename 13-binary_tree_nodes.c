b31f8520cfdd1b6a80e1017d92800a12737ba041
Oludammydy <oludammydy@gmail.com>
2022-08-24 10:06:48 -0700

    move

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,7 +12,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nodes = 0;
 
-	if(tree)
+	if (tree)
 	{
 		nodes += (tree->left || tree->right) ? 1 : 0;
 		nodes += binary_tree_nodes(tree->left);
