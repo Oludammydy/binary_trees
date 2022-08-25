88687e95263be2fd2850896cdc23bf7ef888acb5
lexxyla <bamideleadefolaju@gmail.com>
2022-08-25 16:38:44 +0100

    update headers

--- binary_trees.h
+++ binary_trees.h
@@ -76,4 +76,9 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
+/* helper functions */
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
+
+/*tests*/
+int btic_helper(const binary_tree_t *tree, size_t index, size_t size);
 #endif /* _BINARY_TREES_H_ */
