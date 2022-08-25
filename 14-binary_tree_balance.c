382b262cb30428202009fc73677286d0ff87810d
oludammydy <oludammydy@gmail.com>
2022-08-25 13:37:34 +0100

    added files for task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,10 +8,10 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-  if (tree)
-    return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-  return (0);
+	return (0);
 }
 
 /**
@@ -22,13 +22,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  if (tree)
-  {
-    size_t l = 0, r = 0;
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-    l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-    r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-    return ((l > r) ? l : r);
-  }
-  return (0);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
